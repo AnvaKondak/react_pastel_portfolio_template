dabc754c66e7b0d7e6dec5244804ac1c240ea231
AnvaKondak <anvakondak@gmail.com>
2021-10-07 03:06:33 -0400

    New changes for responsive design.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,12 @@ const Home = () => {
     return ( 
 
      <div className={classes.Home}>
-        <div className={classes.Container}>
+      <div className={classes.Container}>
             <h1 className={classes.Hello}>Hi, I'm Anva{'\n'}</h1>
             <h3>Developer, designer.</h3>
         </div>
         {/* <img className={classes.code} src = {code} alt="code"/> */}
-        <img className={classes.engineer} src = {engineer} alt="code"/>
+        {/* <img className={classes.engineer} src = {engineer} alt="code"/> */}
 
          </div> 
          
